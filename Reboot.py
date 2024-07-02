aeeb7a1d9c4c4469749d58fcac2fb286be528efd
Geovani M Coratti <87933178+geovanimc2005@users.noreply.github.com>
2024-07-02 14:25:30 -0300

    Update Reboot.py

--- Reboot.py
+++ Reboot.py
@@ -2,6 +2,10 @@ import array, os, sys
 import time
 from MySQLdb import *
 import sqlite3 as sql
+from tqdm import *
+import random
+#Autor: Geovani Moura Coratti, Idade: 19 anos
+#Status: Inacabado
 class Reboot:
     def __init__(self):
         self.acionado = False
@@ -11,14 +15,33 @@ class Reboot:
         self.opções = ["Salvar", "Deletar", "Fechar"]
         self.elementos = ["Fogo", "Agua", "Terra", "Ar"]
         self.dicts = {0 : "Calor", 1 : "mutação", 2: "Pacividade", 3 : "Força e imponencia"}
+        self.input_numerico = [str(input(f"Digite A operação Matematica {i}")) for i in ["+", "-", "*", "/"] if self.input == "Matematica"]
+        self.count = 0
+        self.correto = False
+        self.chances = 0
+        self.respostas = ["Não sei", "Dificil dizer"]
+        self.enigmas = {"Como Resolver o meu problema?":"Como Vou saber", "Que dia é Hoje?" : "Problemas"}
+    def fechar(self):
+        return quit()
+    def charadas(self):
+        respostas_input = str(input("Qual é a Resposta ? "))
+        rand = random.randint(0, len(self.enigmas))
+        print(self.enigmas[rand])
+        for i, e in enumerate(self.enigmas):
+            print(i)
+
     def analisador(self, elem : array):
         for i in elem:
             if i == elem[0]:
                 print("Tudo ativado")
         try:
+            if self.input == "Perguntas":
+                self.charadas()
             for i in range(len(self.opções)):
                 if self.input == self.opções[0]:
                     self.save()
+                elif self.input == self.opções[2]:
+                    self.fechar()
         except Exception as exc:
             print(exc)
     def save(self):
